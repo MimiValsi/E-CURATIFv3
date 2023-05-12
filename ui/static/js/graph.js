9c341b423e9ee7757c1c73df961a1e95e8c60879
Miguel DA SILVA <jmiguel.dasilvaf@proton.me>
2023-05-12 08:37:40 +0200

    added billboard.js

--- ui/static/js/graph.js
+++ ui/static/js/graph.js
@@ -1,61 +1,106 @@
+// import bb, {bar} from "./node_modules/billboard.js";
+// async function jsonData() {
+//   const resp = await fetch("http://localhost:3001/jsonGraph");
+//   const jsonData = await resp.json();
+// }
 // import bb, {bar} from "billboard.js";
 
-let nbCuratifs = [];
-for (let i = 0; i < incStruct.length; i++) {
-  nbCuratifs.push(incStruct[i].curatifs);
-}
+// async function jsonData() {
+//   const resp = await fetch("http://localhost:3001/jsonGraph");
+//   const jsData = await resp.json();
+//   const j = JSON.stringify(jsData);
+//   // alert(j);
 
-let nomSources = [];
-for (let i = 0; i < incStruct.length; i++) {
-  nomSources.push(incStruct[i].name);
-}
-// for ESM environment, need to import modules as:
-// import bb, {bar} from "billboard.js";
+//   return j;
+// }
 
-var chart = bb.generate({
-  bindto: "#myPlot",
+(async () => {
+  const getJson = async () => {
+    const response = await fetch("http://localhost:3001/jsonGraph");
+    const data = await response.json();
+    const js = await JSON.stringify(data);
+    const jj = await JSON.parse(js)
 
-  data: {
-    names: {
-      data1: "Curatifs",
-      data2: "Sources"
+    return jj;
+  };
+
+  const jsData = await getJson();
+  // const jsData = JSON.parse(j);
+  // alert(jsData);
+  // alert("jsData length > " + k.length);
+  // console.log(jsData);
+  // console.log("jsData length > " + jsData.length)
+
+  // for (let i = 0; i < jsData.length; i++) {
+  //   console.log(jsData[i].curatifs);
+  // }
+
+  // for (let i = 0; i < jsData.length; i++) {
+  //   console.log(jsData[i].name);
+  // }
+
+  let nbCuratifs = [];
+  for (let i = 0; i < jsData.length; i++) {
+    nbCuratifs.push(jsData[i].curatifs);
+  }
+
+  let nomSources = [];
+  for (let i = 0; i < jsData.length; i++) {
+    nomSources.push(jsData[i].name);
+  }
+
+  // for ESM environment, need to import modules as:
+  // import bb, {bar} from "billboard.js";
+
+  var chart = bb.generate({
+    bindto: "#myPlot",
+
+    data: {
+      names: {
+        data1: "Curatifs",
+        data2: "Sources"
+      },
+      columns: [
+        ["Curatifs en cours", ...nbCuratifs],
+      ],
+      type: "bar", // for ESM specify as: bar()
     },
-    columns: [
-      ["Curatifs en cours", ...nbCuratifs],
-    ],
-    type: "bar", // for ESM specify as: bar()
-  },
 
-  axis: {
-    x: {
-      type: "category",
-      categories: [...nomSources],
-      tick: {
-        rotate: 75,
-        multiline: false
+    axis: {
+      x: {
+        type: "category",
+        categories: [...nomSources],
+        height: 16,
+        tick: {
+          rotate: 75,
+          multiline: false,
+          // fit: false
+        }
+      }
+    },
+
+    size: {
+      width: 1000,
+      height: 400
+    },
+
+    padding: true,
+
+    resize: true,
+
+    zoom: {
+      enabled: true,
+      type: "drag"
+    },
+
+    legend: {
+      position: "inset"
+    },
+
+    bar: {
+      width: {
+        ratio: 0.5
       }
     }
-  },
-
-  size: {
-    width: 1000,
-    height: 400
-  },
-
-  padding: true,
-
-  zoom: {
-    enabled: true,
-    type: "drag"
-  },
-
-  legend: {
-    position: "inset"
-  },
-
-  bar: {
-    width: {
-      ratio: 0.5
-    }
-  }
-});
+  });
+})();
