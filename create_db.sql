cde06bc886688f91f8fc2609e9667454e22ff1cc
MimiValsi <miguel@dasilvaf.net>
2024-09-24 21:26:18 +0200

    create test DB for big import

---
+++ create_db.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE test;
+
+\c test
+
+CREATE TABLE IF NOT EXISTS source (
+	id serial PRIMARY KEY NOT NULL,
+	name varchar,
+	code_gmao varchar
+);
+
+CREATE TABLE IF NOT EXISTS info (
+	id serial not null,
+	agent varchar,
+	ouvrage varchar,
+	priorite integer,
+	detail text,
+	source_id integer,
+	created date,
+	updated date,
+	status varchar,
+	evenement varchar,
+	commentaire varchar,
+	echeance varchar,
+	entite varchar,
+	CONSTRAINT fk_source
+	  FOREIGN KEY(source_id) REFERENCES source(id)
+);
+
+INSERT INTO source (name, code_gmao)
+VALUES ('ALSACE', 'ALSAC');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('AMPERE', 'AMPER');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('ARGENTEUIL', 'ARGE6');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('BILLANCOURT', 'BILLA');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('BOULE', 'BOULE');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('COURBEVOIE', 'CZBEV');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('DANTON', 'DANT5');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('LA BRICHE', 'BRICH');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('LEVALLOIS', 'LEVAL');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('MENUS', 'MENUS');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('NANTERRE', 'NANTE');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('NOVION', 'NOVIO');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('PUTEAUX', 'PUTEA');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('RUEIL', 'RUEIL');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('TILLIERS', 'TILLI');
+
+INSERT INTO source (name, code_gmao)
+VALUES ('SAINT OUEN', 'SSOU5');
