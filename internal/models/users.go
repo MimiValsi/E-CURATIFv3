8e08e4592e2352bdd17cf2628fe25dbc17270e1f
Miguel Da Silva <miguel@dasilvaf.net>
2024-10-06 21:35:40 +0200

    Add user signup and login models

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,11 +2,15 @@ package models
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"golang.org/x/crypto/bcrypt"
+
+	"E-CURATIFv3/database"
 )
 
 type User struct {
@@ -19,10 +23,10 @@ type User struct {
 	DB *pgxpool.Pool
 }
 
-func (m *User) Insert(conn *pgxpool.Conn) error {
+func (u *User) Insert(conn *pgxpool.Conn) error {
 	ctx := context.Background()
 
-	hashedPasswd, err := bcrypt.GenerateFromPassword(m.HashedPassword, 12)
+	hashedPasswd, err := bcrypt.GenerateFromPassword(u.HashedPassword, 12)
 	if err != nil {
 		return err
 	}
@@ -34,7 +38,7 @@ RETURNING id;
 	`
 
 	id := 0
-	err = conn.QueryRow(ctx, query, m.Name, m.Email, string(hashedPasswd)).Scan(&id)
+	err = conn.QueryRow(ctx, query, u.Name, u.Email, string(hashedPasswd)).Scan(&id)
 	if err != nil {
 		return err
 	}
@@ -44,10 +48,34 @@ RETURNING id;
 
 }
 
-func (m *User) Authenticate(email, password string) (int, error) {
-	return 0, nil
+func (u *User) Authenticate(conn *pgxpool.Conn) (int, error) {
+	ctx := context.Background()
+
+	var hashed_passwd []byte
+
+	query := `SELECT id, hashed_password FROM users WHERE email = $1`
+
+	err := conn.QueryRow(ctx, query, u.Email).Scan(&u.ID, &hashed_passwd)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			fmt.Println("pas bon du tout")
+			return 0, database.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashed_passwd, u.HashedPassword)
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, database.ErrInvalidCredentials
+		}
+		return 0, err
+	}
+
+	return u.ID, nil
 }
 
-func (m *User) Exists(id int) (bool, error) {
+func (u *User) Exists(id int) (bool, error) {
 	return false, nil
 }
