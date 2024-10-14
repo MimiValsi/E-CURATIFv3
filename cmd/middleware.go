14314f57803eea0cd0bec31060ab3c604797451c
MimiValsi <miguel@dasilvaf.net>
2024-10-14 08:30:17 +0200

    Add cors security. Still in development

--- cmd/middleware.go
+++ cmd/middleware.go
@@ -10,7 +10,10 @@ import (
 
 func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		// need to find a solution to allow JS!
+		// w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self'; font-src 'self'")
+		// w.Header().Set("Content-Security-Policy", "script-src 'https://localhost:8080/'")
+		// w.Header().Set("Content-Security-Policy", "")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
@@ -100,3 +103,21 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// func (app *application) enableCORS(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Add("Vary", "Origin")
+//
+// 		origin := r.Header.Get("Origin")
+// 		if origin != "" {
+// 			for i := range app.cors.trustedOrigins {
+// 				if origin == app.cors.trustedOrigins[i] {
+// 					w.Header().Set("Access-Control-Allow-Origin", origin)
+// 					break
+// 				}
+// 			}
+// 		}
+//
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
