7534ad3f3268aa03bb5431f88de8084811f9a197
Miguel Da Silva <miguel@dasilvaf.net>
2024-10-18 18:13:10 +0200

    Create info table search

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -2,53 +2,55 @@
 // To avoid missclick, ask to confirm the delete
 let del = document.getElementById("check-delete");
 if (del) {
-  del.onclick = function () {
+  del.onclick = () => {
     return confirm("Êtes-vous sûr(e) de supprimer?");
   };
 }
 
 let getFile = document.getElementById("getFile");
 let imprt = document.getElementById("importCSV");
-imprt.onchange = () => {
-  getFile.submit();
-  alert("Fichier en cours de transfert...");
-};
+if (getFile && imprt) {
+  imprt.onchange = () => {
+    getFile.submit();
+    alert("Fichier en cours de transfert...");
+  };
+}
 
-function search_status() {
-  document
-    .getElementById("search_status")
-    .addEventListener("keyup", function () {
-      let input, filter, table, tr, td, i, txtValue;
+let input = document.getElementById("search_info");
 
-      // Fetch for the <input> searcg tag
-      input = document.getElementById("search_status");
-      filter = input.value.toUpperCase(); // It simplifies the research
-      table = document.getElementById("myTable");
-      tr = table.getElementsByTagName("tr"); // Fetch every <tr> inside table
+if (input) {
+  input.addEventListener("input", function () {
+    let table = document.getElementById("info_table");
+    let noMatchMessage = document.getElementById("noMatch");
+    let rows = table.getElementsByTagName("tr");
+    let filter = input.value.toLowerCase();
+    let matchFound = false;
 
-      /* Inside "sourceView" <table id="myTable"> there's 2 <tr>
-       * tr[1] contains 2 <td>. We're going to look inside td[1]
-       * which represents "Status" column.
-       * And coz td[0] doesn't work... Added to TODO list
-       */
+    for (let i = 1; i < rows.length; i++) {
+      let row = rows[i];
+      let cells = row.getElementsByTagName("td");
+      let found = false;
 
-      for (i = 1; i < tr.length; i++) {
-        // For Info search change to ...("td")[0];
-        // For Priority search change to ...("td")[1];
-        // For Status search change to ...("td")[2];
-        td = tr[i].getElementsByTagName("td")[0];
-        //                               here ^
-
-        if (td) {
-          txtValue = td.textContent;
-          console.log(txtValue);
-
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-          } else {
-            tr[i].style.display = "none";
-          }
+      for (let j = 0; j < cells.length; j++) {
+        let cell = cells[j];
+        if (cell.textContent.toLowerCase().indexOf(filter) > -1) {
+          found = true;
+          matchFound = true;
+          break;
         }
       }
-    });
+
+      if (found) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    }
+
+    if (!matchFound) {
+      noMatchMessage.style.display = "block";
+    } else {
+      noMatchMessage.style.display = "none";
+    }
+  });
 }
