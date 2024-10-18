b09805c0bc3a048832a3c58420a4374c4cee4ae2
Miguel Da Silva <miguel@dasilvaf.net>
2024-10-18 16:16:21 +0200

    Add auto submit for csv file

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,44 +1,54 @@
-// onclick='return confirm("Êtes-vous sûr de supprimer?");'
+// It's used in info and source view page.
+// To avoid missclick, ask to confirm the delete
+let del = document.getElementById("check-delete");
+if (del) {
+  del.onclick = function () {
+    return confirm("Êtes-vous sûr(e) de supprimer?");
+  };
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-  let d = document.getElementById("delete_info");
-  if (d) {
-    console.log("exists");
-  }
-});
+let getFile = document.getElementById("getFile");
+let imprt = document.getElementById("importCSV");
+imprt.onchange = () => {
+  getFile.submit();
+  alert("Fichier en cours de transfert...");
+};
 
-//function search_status() {
-//document.getElementById("search_status").addEventListener("keyup", function () {
-//  let input, filter, table, tr, td, i, txtValue;
-//
-//  // Fetch for the <input> searcg tag
-//  input = document.getElementById("search_status");
-//  filter = input.value.toUpperCase(); // It simplifies the research
-//  table = document.getElementById("myTable");
-//  tr = table.getElementsByTagName("tr"); // Fetch every <tr> inside table
-//
-//  /* Inside "sourceView" <table id="myTable"> there's 2 <tr>
-//   * tr[1] contains 2 <td>. We're going to look inside td[1]
-//   * which represents "Status" column.
-//   * And coz td[0] doesn't work... Added to TODO list
-//   */
-//
-//  for (i = 1; i < tr.length; i++) {
-//    // For Info search change to ...("td")[0];
-//    // For Priority search change to ...("td")[1];
-//    // For Status search change to ...("td")[2];
-//    td = tr[i].getElementsByTagName("td")[2];
-//    //                               here ^
-//
-//    if (td) {
-//      txtValue = td.textContent;
-//      console.log(txtValue);
-//
-//      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//        tr[i].style.display = "";
-//      } else {
-//        tr[i].style.display = "none";
-//      }
-//    }
-//  }
-//});
+function search_status() {
+  document
+    .getElementById("search_status")
+    .addEventListener("keyup", function () {
+      let input, filter, table, tr, td, i, txtValue;
+
+      // Fetch for the <input> searcg tag
+      input = document.getElementById("search_status");
+      filter = input.value.toUpperCase(); // It simplifies the research
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr"); // Fetch every <tr> inside table
+
+      /* Inside "sourceView" <table id="myTable"> there's 2 <tr>
+       * tr[1] contains 2 <td>. We're going to look inside td[1]
+       * which represents "Status" column.
+       * And coz td[0] doesn't work... Added to TODO list
+       */
+
+      for (i = 1; i < tr.length; i++) {
+        // For Info search change to ...("td")[0];
+        // For Priority search change to ...("td")[1];
+        // For Status search change to ...("td")[2];
+        td = tr[i].getElementsByTagName("td")[0];
+        //                               here ^
+
+        if (td) {
+          txtValue = td.textContent;
+          console.log(txtValue);
+
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    });
+}
